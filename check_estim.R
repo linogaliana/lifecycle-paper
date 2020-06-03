90f14583e1fb2b556ed25cacba923174170fd547
Lino Galiana <lino.galiana@insee.fr>
2020-06-03 21:30:33 +0200

    check estimates

---
+++ check_estim.R
@@ -0,0 +1,73 @@
+library(tablelight)
+
+N_moments = 6L
+
+
+
+unzip("~/Destinie.zip", exdir="~")
+unzip("~/Enquete Patrimoine.zip", exdir="~")
+
+
+
+EP_data <- wealthyR:::prepare_inheritance_sample(
+  path_survey =  "~/Enquete Patrimoine"
+)
+
+# MODEL 1: INTERVAL REGRESSION ---------------
+
+inheritance_data <- REtage::prepare_estimation(EP_data)
+
+
+bounds <- c(3,8,15,30,60,100,150,200,250)*1000
+lbounds <- log(bounds)
+
+estim_data <- inheritance_data[get('revenu')>0]
+
+estim_data[,'MTHER' := as.numeric(as.character(MTHER))]
+estim_data <- estim_data[order(MTHER)]
+
+
+inheritance_model <- REtage::ordered_model_threshold(
+  data = data.frame(estim_data[order(MTHER)]),
+  formula = "MTHER ~ lw",
+  link = "probit",
+  constantSD = TRUE,
+  thresholds = lbounds
+)
+
+summary(inheritance_model)
+
+
+
+inheritance_model <- REtage::ordered_model_threshold(
+  data = data.frame(estim_data[order(MTHER)]),
+  formula = "MTHER ~ lw + tr_agfinetu",
+  link = "probit",
+  constantSD = TRUE,
+  thresholds = lbounds
+)
+
+tablelight::view_html(
+  tablelight::light_table(
+    inheritance_model, type = "html"
+  )
+)
+
+
+
+
+
+inheritance_model <- REtage::ordered_model_threshold(
+  data = data.frame(estim_data[AGFINETU > 20][order(MTHER)]),
+  formula = "MTHER ~ lw + tr_agfinetu",
+  link = "probit",
+  constantSD = TRUE,
+  thresholds = lbounds
+)
+
+tablelight::view_html(
+  tablelight::light_table(
+    inheritance_model, type = "html",
+    stats.list = c("ll","n","sigma")
+  )
+)
