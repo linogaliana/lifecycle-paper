d254d02bf883baee74e27be5cfc3e1a99ce74ee5
Lino Galiana <lino.galiana@insee.fr>
2020-11-25 11:28:53 +0100

    evaluate loss function

---
+++ eval_loss_function.R
@@ -0,0 +1,21 @@
+library(data.table)
+library(capitulation)
+library(ggplot2)
+
+source("import_functions.R")
+
+r <- 0.03
+beta <- 0.95
+gamma <- 0.5
+
+epseps <- report_epsilon(
+  # theta = c(r = r,
+  #           beta = beta,
+  #           gamma = gamma),
+  r = r,
+  beta = beta,
+  gamma = gamma,
+  population = population,
+  EP_2015 = EP_2015,
+  EP_2018 = EP_2018,
+  EP_lon = EP_lon)
