a9da7f1e5f36115d906c3afd6e1c193aa41163b4
Lino Galiana <lino.galiana@insee.fr>
2020-11-26 16:22:49 +0100

    prepare la grid search

--- eval_loss_function.R
+++ eval_loss_function.R
@@ -21,3 +21,39 @@ epseps <- report_epsilon(
   EP_lon = EP_lon,
   verbose = TRUE)
 epseps
+
+
+
+
+theta <- c(beta = '0.975', gamma = '0.6')
+
+
+betas <- seq(0.9, 1, by = 0.005)
+gammas <- seq(0.4,0.6, by = 0.02)
+
+grid <- data.frame(
+  expand.grid(beta = betas[1:5], gamma = gammas[1:5])
+)
+
+grid <- split(grid, 1:nrow(grid))
+
+plot_theta <- lapply(grid, function(theta){
+  epseps <- report_epsilon(
+    # theta = c(r = r,
+    #           beta = beta,
+    #           gamma = gamma),
+    r = r,
+    beta = theta$beta,
+    gamma = theta$gamma,
+    population = population,
+    EP_2015 = EP_2015,
+    EP_2018 = EP_2018,
+    EP_lon = EP_lon,
+    verbose = FALSE)
+  return(
+    data.frame(beta = theta$beta,
+               gamma = theta$gamma,
+               loss = epseps)
+  )
+})
+
