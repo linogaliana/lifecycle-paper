76805c500191dd2f2863af5840e3a263ce0fd2a2
Lino Galiana <lino.galiana@insee.fr>
2020-12-02 10:02:03 +0100

    clean script output

--- stats_output.R
+++ stats_output.R
@@ -1,4 +1,19 @@
+
+# PARAMETERS GLOBAUX -------------------------
+
 population <- readRDS(file = "~/estimation/tempfile.rds")
+data <- readRDS("~/estimation/data.rds")
+EP_2015 <- data[['EP_2015']]
+EP_2018 <- data[['EP_2018']]
+EP_lon <- data[['EP_lon']]
+
+
+beta  <- 0.9724306
+gamma <- 0.8435442
+
+
+# SIMULATE MODEL -----------------------------
+
 
 simulations <- capitulation::life_cycle_model(
   population,
@@ -14,35 +29,69 @@ simulations <- capitulation::life_cycle_model(
   get_capital_income = TRUE, additional_vars = c("tr_age_2015","sexe","findet"))
 
 
-simulations[annee == 2015, as.list(summary(wealth)), by="sexe"]
+# UN PEU DE DATA CLEANING --------------------
 
+simulations[, 'SEXE' := data.table::fifelse(get('sexe')==1,
+                                            'Homme',
+                                            'Femme')]
 simulations[, 'tr_diplome' := cut(findet, breaks = c(min(findet), 16,18,21,25, max(findet)), include.lowest = TRUE)]
+simulations[, 'decile_w' := cut(revenu,quantile(revenu,probs= 0:10/10), labels = 1:10, include.lowest = TRUE)]
 simulations[, 'decile_y' := cut(Y,quantile(Y,probs= 0:10/10), labels = 1:10, include.lowest = TRUE)]
 
 
-simulations[annee == 2015, as.list(summary(wealth)), by="sexe"]
-simulations[annee == 2015, as.list(summary(wealth)), by="tr_diplome"][order(tr_diplome)]
+write_stats <- function(data, wealth_var = "wealth"){
+  
+  
+  data.table::fwrite(
+    simulations[annee == 2015, as.list(round(summary(get(wealth_var)))), by="SEXE"],
+    file = "./stats/stats_sexe.csv"
+  )
+  
+  
+  
+  data.table::fwrite(
+    simulations[annee == 2015, as.list(round(summary(get(wealth_var)))), by="tr_diplome"][order(tr_diplome)],
+    file = "./stats/stats_diplome.csv"
+  )
+  
+  
+  data.table::fwrite(
+    simulations[annee == 2015, as.list(round(summary(get(wealth_var)))), keyby=decile_w][order(decile_w)],
+    file = "./stats/stats_income_labor.csv"
+  )
+  
+  data.table::fwrite(
+    simulations[annee == 2015, as.list(round(summary(get(wealth_var)))), keyby=decile_y][order(decile_y)],
+    file = "./stats/stats_income_total.csv"
+  )
+  
+}
 
-simulations[annee == 2015, as.list(round(summary(wealth))), keyby=decile_w][order(decile_w)]
-simulations[annee == 2015, as.list(round(summary(wealth)), keyby=decile_y][order(decile_y)]
+
+EP_2015
 
 
-tempdf <- simulations[,.('med' = median(wealth),
-                         'mean' = mean(wealth)),by = annee]
-tempdf <- data.table::melt(tempdf, id.vars = "annee")
-
-# PAR ANNEE ------
-
+# GRAPHIQUE PAR ANNEE --------------------------------
 
 library(ggplot2)
 library(data.table)
 
+tempdf <- simulations[,.('median wealth' = median(wealth),
+                         'mean wealth' = mean(wealth)),by = annee]
+tempdf <- data.table::melt(tempdf, id.vars = "annee")
 
-ggplot(tempdf[annee>=2009 & annee<=2040]) + geom_line(aes(x = annee, y = value, color = variable))
+
+p1 <- ggplot(tempdf[annee>=2009 & annee<=2040]) + geom_line(aes(x = annee, y = value/1000, color = variable)) +
+  labs(x = "Year", y = "Wealth (thousand euros)", color = NULL) +
+  theme(legend.position = "bottom")
+ggsave(plot = p1, "./stats/plot_wealth_evolution.pdf")
 
 
 
-# PAR AGE ------
+
+
+# CHUTES --------------------------------
+
 
 tempdf <- simulations[,.('med' = median(wealth),
                          'mean' = mean(wealth)),by = c("annee", "age")]
@@ -50,4 +99,4 @@ tempdf <- data.table::melt(tempdf, id.vars = c("annee","age"))
 
 
 ggplot(tempdf[annee>=2009 & annee<=2040 & age %between% c(25,80) & variable == "med"]) +
-  geom_smooth(aes(x = age, y = value, color = factor(annee), linetype = variable), se = FALSE)
+  geom_smooth(aes(x = age, y = value, color = factor(annee)), se = FALSE)
