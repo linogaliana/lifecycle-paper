17ebd9d5032314a740e7bca4996fe0a84bf8049c
Lino Galiana <lino.galiana@insee.fr>
2020-12-15 16:24:39 +0100

    update script heterogeneity

--- script_heterogeneity.R
+++ script_heterogeneity.R
@@ -37,10 +37,13 @@ saveRDS(data, "~/estimation/data.rds")
 
 data_prediction <- capitulation::prepare_data(
   path_data = "~",
-  inheritance_model = inheritance_model
+  inheritance_model = inheritance_model,
+  time_0 = "birth"
 )
 # aws.s3::s3saveRDS(data_prediction, "data_prediction.rds",
 #                   bucket = "groupe-788")
+
+data_prediction <- data_prediction[age>findet]
 
 menages_structural2 <- data.table::copy(data_prediction)
 menages_structural2[,'hg' := get('H_given')]
@@ -78,15 +81,18 @@ menages_structural2[,'SEXE' := as.numeric(as.character(get("SEXE")))]
 EP_2015[,'SEXE' := as.numeric(as.character(get("SEXE")))]
 EP_2018[,'SEXE' := as.numeric(as.character(get("SEXE")))]
 EP_lon[,'SEXE' := as.numeric(as.character(get("SEXE")))]
+EP_lon[,'SEXE' := get('SEXE')-1]
+
+# menages_structural2 <- menages_structural2[age>findet]
 
 output <- mindist::estimation_theta(
   theta_0 = c("beta" = {if(is.null(beta)) beta_0 else NULL},
-              "gamma.parameters" = gamma_0,
+              "gamma.parameters" = c(gamma_0, 0),
               "r" = {if(is.null(r)) 0.03 else NULL}
   ),
   beta = beta,
   r = r,
-  gamma = "gamma ~ 0 + SEXE",
+  gamma = "gamma ~ SEXE",
   model_function = mindist:::loss_function,
   prediction_function = wealthyR:::model_capitulation,
   approach = estimation_method,
@@ -94,10 +100,10 @@ output <- mindist::estimation_theta(
   EP_2015 = EP_2015,
   EP_lon = EP_lon,
   EP_2018 = EP_2018,
-  data_microsimulated = menages_structural2,
+  data_microsimulated = data.table::copy(menages_structural2),
   N_moments = 180,
   by = c("AGE", "tr_age_2015"),
-  # moment1 = "share",
+  moment1 = "share",
   scale = scale_wealth,
   moments_weights = "weight",
   verbose = TRUE,
@@ -105,4 +111,28 @@ output <- mindist::estimation_theta(
   Hreceived_var = "hr",
   method = "Nelder-Mead",
   additional_vars = "SEXE"
+)
+
+class(output) <- c("mindist", "list")
+
+tablelight::view_html(tablelight::light_table(output, type = "html",
+                                              covariate.labels = c("$\\beta$",
+                                                                   "Male $\\gamma$",
+                                                                   "Difference between Male and Female $\\gamma$"),
+                                              dep.var.labels = "\\textsc{Estimates}",
+                                              column.labels = NULL))
+
+
+
+cat(
+  tablelight::light_table(output, type = "latex",
+                          covariate.labels = c("$\\beta$",
+                                               "Male $\\gamma$",
+                                               "Difference between Male and Female $\\gamma$"),
+                          dep.var.labels = "\\textsc{Estimates}", column.labels = NULL,
+                          title = "Estimation results  (model with heterogeneity on risk aversion)",
+                          add.lines = "Model estimated by minimum distance using aggregate moments from microsimulated data and wealth surveys. Moments in wealth survey are defined in Table \\ref{tab:data}. The same moments are computed in microsimulated data.",
+                          label = "tab: estimation table gamma"),
+  sep = "\n",
+  file = "~/5cece6ccccdef65e149a3774/tables/resultsGMM.tex"
 )
