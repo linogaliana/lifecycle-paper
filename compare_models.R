2fc371e35ad3de2a233acb13352c0665a08d2ca3
Galiana Lino <W3CRK9@ad.insee.intra>
2021-01-14 14:05:06 +0100

    un petit script pour update colonnes

--- compare_models.R
+++ compare_models.R
@@ -1,3 +1,21 @@
 output_r2 <- readRDS("./output_r02.rds")
 output_r3 <- readRDS("./output.rds")
 output_r4 <- readRDS("./output_r4.rds")
+
+tablelight::view_html(
+  tablelight::light_table(
+    list(output_r2, output_r3, output_r4),
+    type = "html",
+    column.labels = c("r=2%", "r=3%", "r=4%")
+  )
+)
+
+print_delta <- function(model, r){
+  beta <- as.numeric(model$estimates$theta_hat['beta'])
+  delta <- 1/beta - 1
+  return(c("delta" = delta, "r - delta" = r - delta))
+}
+print_delta(output_r2, .02)
+print_delta(output_r3, .03)
+print_delta(output_r4, .04)
+
